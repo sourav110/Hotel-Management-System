6250dea9f17b50908a51550a2d0e03c8efed3eab
sourav110 <sourav.csv@gmail.com>
2020-05-15 14:27:22 +0600

    template integration

---
+++ HMS/Code/CustomVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class CustomVariables
+    {
+        public const string HotelFullName = "Hotel Management System";
+        public const string HotelShortName = "Hotel Name";
+        public const string HotelAbbreviatedName = "HMS";
+    }
+}
